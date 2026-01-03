e6a219868bd51bd620aeb0016447877dda33b990
yuanfangchuanlaifengdi <shulonghong@gmail.com>
2026-01-03 15:39:58 +0800

    feat(init): 初始化

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/styles/main.scss";`, // 全局引入
+        },
+      },
+    },
+  }),
+)
